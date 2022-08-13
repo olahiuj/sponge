beb301d671c9bd1f6a0e08584da148127ef90d44
jpwang <jpwang@smail.nju.edu.cn>
2022-08-14 01:29:25 +0800

    impl: lab2-wrap_unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,17 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32{static_cast<uint32_t>(n) + isn.raw_value()};
+}
+
+static uint64_t dist(uint64_t x, uint64_t checkpoint) {
+    return (x > checkpoint) ? (x - checkpoint) : (checkpoint - x);
+}
+
+static void update(uint64_t &ans, uint64_t checkpoint, uint64_t cand) {
+    if (dist(ans, checkpoint) > dist(cand, checkpoint)) {
+        ans = cand;
+    }
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +38,20 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    constexpr uint64_t MASK  = 0xFFFFFFFF00000000ULL;
+    constexpr uint64_t DELTA = 0x0000000100000000ULL;
+
+    uint64_t delta = n.raw_value() - isn.raw_value();
+    // abs_seq - delta = k * 2^32, where k∈ℕ
+
+    auto pos = checkpoint & MASK;
+    uint64_t ans{pos + delta};
+
+    if (pos > 0) {
+        update(ans, checkpoint, pos - DELTA + delta);
+    }
+    if (pos != MASK) {
+        update(ans, checkpoint, pos + DELTA + delta);
+    }
+    return ans;
 }
